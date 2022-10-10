4e7f6e353a6e9cd43cab71cdd9df5e6163e3cbb1
Hizkiel-Mesfin <mesfinezekiel@gmail.com>
2022-10-10 18:45:03 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from "react"; //Main React.js library
 import ReactDOM from "react-dom/client"; //we use ReactDOM to render into the DOM
 
-// only update the value of this array
-const navlinkItems = [];
+const linksArr = [
+  { hyper: "https://www.google.com", name: " Link to google.com" },
+  { hyper: "https://www.facebook.com", name: "Link to facebook.com" },
+  { hyper: "https://www.amazon.com", name: " Link to amazon.com" },
+];
+
+const navlinkItems = linksArr.map((link, i) => {
+  return (
+    <li key={i} className="nav-item">
+      <a className="nav-link" href={link.hyper}>
+        {link.name}
+      </a>
+    </li>
+  );
+});
+
+console.log(navlinkItems);
 
 const content = <ul className="nav">{navlinkItems}</ul>;
 
